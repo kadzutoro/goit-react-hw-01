411b9e462f3baea7d493422febeaf65ef91bf00d
kadzutoro <nikitashpir132@gmail.com>
2024-02-19 00:15:32 +0200

    u

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,34 @@
+function Profile({name, tag, location, image, stats: {followers, likes, views}}) {
+    return (
+    <div>
+  <div>
+    <img
+      src={image}
+      alt={name}
+    />
+    <p>{name}</p>
+    <p>@{tag}</p>
+    <p>{location}</p>
+  </div>
+
+  <ul>
+    <li>
+      <span>Followers</span>
+      <span>{followers}</span>
+    </li>
+    <li>
+      <span>Views</span>
+      <span>{views}</span>
+    </li>
+    <li>
+      <span>Likes</span>
+      <span>{likes}</span>
+    </li>
+  </ul>
+</div>
+)
+} 
+
+export default Profile
+
+
